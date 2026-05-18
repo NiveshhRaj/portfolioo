7be9b67bbcb65d755b8648dcc0de142c66134306
NiveshhRaj <niveshhraj212004@gmail.com>
2026-05-18 14:27:19 +0530

    latest commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-    baseURL: "https://portfolio-backend-zv3z.onrender.com/api"
+    baseURL: import.meta.env.VITE_API_URL;
 })
 
 export default API
