b6ba245cdcad42abf78d14f753e951e8aaa741b6
NiveshhRaj <niveshhraj212004@gmail.com>
2026-05-18 15:06:14 +0530

    modified security

--- backend/src/main/java/com/example/portfolio/portfolio/security/SecurityConfig.java
+++ backend/src/main/java/com/example/portfolio/portfolio/security/SecurityConfig.java
@@ -3,21 +3,32 @@ package com.example.portfolio.portfolio.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+    public SecurityFilterChain securityFilterChain(
+
+            HttpSecurity http
+
+    ) throws Exception {
 
         http
-                .csrf(AbstractHttpConfigurer::disable)
+
+                .csrf(csrf -> csrf.disable())
+
+                .cors(cors -> {})
+
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+
+                        .anyRequest()
+
+                        .permitAll()
                 );
 
         return http.build();
     }
+}
 }
