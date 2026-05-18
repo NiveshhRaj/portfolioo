f056327fda7c08a5c26485a8a2b1f0e9d760a568
NiveshhRaj <niveshhraj212004@gmail.com>
2026-05-18 14:22:01 +0530

    updated production cors

--- backend/src/main/java/com/example/portfolio/portfolio/config/CorsConfig.java
+++ backend/src/main/java/com/example/portfolio/portfolio/config/CorsConfig.java
@@ -2,9 +2,14 @@ package com.example.portfolio.portfolio.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.web.cors.CorsConfiguration;
+
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
@@ -12,16 +17,44 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
 
-        CorsConfiguration config = new CorsConfiguration();
+        CorsConfiguration config =
+                new CorsConfiguration();
 
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+
+        config.setAllowedOrigins(
+
+                List.of(
+
+                        "http://localhost:5173",
+
+                        "https://portfolio-backend-zv3z.onrender.com/"
+                )
+        );
+
+        config.setAllowedHeaders(
+
+                List.of("*")
+        );
+
+        config.setAllowedMethods(
+
+                List.of(
+                        "GET",
+                        "POST",
+                        "PUT",
+                        "DELETE",
+                        "OPTIONS"
+                )
+        );
 
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
 
-        source.registerCorsConfiguration("/**", config);
+        source.registerCorsConfiguration(
+                "/**",
+                config
+        );
 
         return new CorsFilter(source);
     }
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-    baseURL: "http://localhost:8081/api"
+    baseURL: "https://portfolio-backend-zv3z.onrender.com/api"
 })
 
 export default API
